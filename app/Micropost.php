684fec0c2e9b6d91a6ee99ea44ead912c52ee3b4
tsukada <f117karasu@gmail.com>
2019-10-28 05:08:36 +0000

    favorite

--- app/Micropost.php
+++ app/Micropost.php
@@ -6,10 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Micropost extends Model
 {
-    protected $fillable = ['content', 'user_id'];
+    protected $fillable = ['content', 'user_id', 'micropost_id'];
     
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+    
+    public function favorite_users()
+    {
+        return $this->belongsToMany(User::class, 'microposts', 'micropost_id', 'user_id')->withTimestamps();
     }
 }
